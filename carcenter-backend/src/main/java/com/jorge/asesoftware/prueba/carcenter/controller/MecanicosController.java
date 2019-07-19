d9b189f777157527853f6f5544fdf7748e8bc0b0
Jorge Eliecer Sierra Torres <jorge.sierra@sbd.com.co>
2019-07-19 17:09:51 -0500

    se agrega CORS

--- carcenter-backend/src/main/java/com/jorge/asesoftware/prueba/carcenter/controller/MecanicosController.java
+++ carcenter-backend/src/main/java/com/jorge/asesoftware/prueba/carcenter/controller/MecanicosController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,7 @@ import com.jorge.asesoftware.prueba.carcenter.DAO.MecanicosRepository;
 import com.jorge.asesoftware.prueba.carcenter.DTO.Mecanicos;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class MecanicosController {
 	
 	@Autowired
